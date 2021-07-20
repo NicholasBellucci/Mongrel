b7cf292bf69a0f9f25a70840b4fb98b76ee2a9c9
Nicholas Bellucci <nickdbellucci@gmail.com>
2021-07-20 18:35:11 -0400

    Added output docs

--- Sources/Mongrel/HTML/Elements/General/Output.swift
+++ Sources/Mongrel/HTML/Elements/General/Output.swift
@@ -1,10 +1,7 @@
-//
-//  Output.swift
-//
-//
-//  Created by Nicholas Bellucci on 7/19/21.
-//
-
+/// The ``Output`` element represents the results of a calculation.
+///
+/// For more information about the ``<meta>`` tag,
+/// visit https://www.w3schools.com/tags/tag_output.asp
 public struct Output: Attributable {
     public var tag: String = "output"
     public var attributes: [String: String] = [:]
@@ -12,37 +9,31 @@ public struct Output: Attributable {
 
     private var innerHTML: String = ""
 
-    public init() { }
+    /// Creates an output element with a name and relationship ids.
+    ///
+    /// - Parameters:
+    ///   - name: The name of the output.
+    ///   - ids: The element ids of the relating elements.
+    ///   - content: The ``HTMLConvertible`` elements that will make up
+    ///   the HTML inside of the `<output></output>` tags.
+    public init(name: String, for ids: [String], @HTMLBuilder _ content: () -> HTMLConvertible) {
+        attributes["name"] = name
+        attributes["for"] = ids
+            .sorted()
+            .map { $0 }.joined(separator: " ")
 
-    public init(@HTMLBuilder _ content: () -> HTMLConvertible) {
         innerHTML = content().stringValue
     }
 }
 
 public extension Output {
-    func `for`(_ ids: String...) -> Attributable {
-        var copy = self
-
-        copy.attributes["for"] = ids
-            .sorted()
-            .map { $0 }.joined(separator: " ")
-
-        return copy
-    }
-
+    /// Sets the button's attribute: `form`.
+    ///
+    /// - Parameters:
+    ///   - id: The id of the form in which the output belongs.
     func formId(_ id: String) -> Output {
         var copy = self
         copy.attributes["form"] = id
-        return copy
-    }
-
-    /// Sets the button's attribute: `name`.
-    ///
-    /// - Parameters:
-    ///   - name: The name of the output.
-    func name(_ name: String) -> Output {
-        var copy = self
-        copy.attributes["name"] = name
         return copy
     }
 }
