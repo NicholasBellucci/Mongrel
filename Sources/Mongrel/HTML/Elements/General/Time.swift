d1c7ead334ddcd1a8d89325b314bc31ee01013e5
Nicholas Bellucci <nickdbellucci@gmail.com>
2021-07-20 19:19:34 -0400

    Added time docs

--- Sources/Mongrel/HTML/Elements/General/Time.swift
+++ Sources/Mongrel/HTML/Elements/General/Time.swift
@@ -1,15 +1,16 @@
-//
-//  Time.swift
-//  
-//
-//  Created by Nicholas Bellucci on 7/19/21.
-//
-
+/// The ``Time`` element defines a specific time or datetime.
+///
+/// For more information about the ``<time>`` tag,
+/// visit https://www.w3schools.com/tags/tag_time.asp
 public struct Time: Attributable, EventListener {
     public var tag: String = "time"
     public var attributes: [String: String] = [:]
     public var styles: [String: String] = [:]
 
+    /// Creates a time element with an optional format.
+    ///
+    /// - Parameters:
+    ///   - format: The machine-readable format.
     public init(format: String? = nil) {
         if let format = format {
             attributes["datetime"] = format
