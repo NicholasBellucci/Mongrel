017f8763d651607f8f00b5103b73d208fc3cf019
Nicholas Bellucci <nickdbellucci@gmail.com>
2021-07-20 21:00:52 -0400

    Added complete iframe docs

--- Sources/Mongrel/HTML/Elements/Layout/iFrame.swift
+++ Sources/Mongrel/HTML/Elements/Layout/iFrame.swift
@@ -35,30 +35,51 @@ public struct iFrame: Attributable, EventListener {
 }
 
 public extension iFrame {
+    /// Sets the iframe's attribute: ``allow``.
+    ///
+    /// - Parameters:
+    ///   - policy: The feature policy.
     func allow(_ policy: String) -> iFrame {
         var copy = self
         copy.attributes["allow"] = policy
         return copy
     }
 
+    /// Sets the iframe's attribute: ``allowfullscreen``.
+    ///
+    /// - Parameters:
+    ///   - value: Determines if fullscreen mode can be activated.
+    ///   by calling ``requestFullscreen()``.
     func allowFullscreen(_ value: Bool) -> iFrame {
         var copy = self
         copy.attributes["allowfullscreen"] = "\(value)"
         return copy
     }
 
+    /// Sets the iframe's attribute: ``allowpaymentrequest``.
+    ///
+    /// - Parameters:
+    ///   - value: Determines if invoking the Payment Request API is allowed.
     func allowPaymentRequest(_ value: Bool) -> iFrame {
         var copy = self
         copy.attributes["allowpaymentrequest"] = "\(value)"
         return copy
     }
 
+    /// Sets the iframe's attribute: ``height``.
+    ///
+    /// - Parameters:
+    ///   - height: The height of the element, in pixels.
     func height(_ height: Double) -> iFrame {
         var copy = self
         copy.attributes["height"] = "\(height)"
         return copy
     }
 
+    /// Sets the iframe's attribute: ``loading``.
+    ///
+    /// - Parameters:
+    ///   - type: The type of load either ``eager`` or ``lazy``.
     func loadType(_ type: LoadType) -> iFrame {
         var copy = self
         copy.attributes["loading"] = type.rawValue
@@ -75,12 +96,20 @@ public extension iFrame {
         return copy
     }
 
+    /// Sets the iframe's attribute: ``referrerpolicy``.
+    ///
+    /// - Parameters:
+    ///   - policy: The referrer information to send when fetching the iframe.
     func referrerPolicy(_ policy: ReferrerPolicy) -> iFrame {
         var copy = self
         copy.attributes["referrerpolicy"] = policy.rawValue
         return copy
     }
 
+    /// Sets the iframe's attribute: ``sandbox``.
+    ///
+    /// - Parameters:
+    ///   - types: The types of restictions for the iframe's content.
     func sandbox(_ types: SandboxType...) -> iFrame {
         var copy = self
 
@@ -91,12 +120,10 @@ public extension iFrame {
         return copy
     }
 
-    func srcdoc(@HTMLBuilder _ content: () -> HTMLConvertible) -> iFrame {
-        var copy = self
-        copy.attributes["srcdoc"] = String(describing: content())
-        return copy
-    }
-
+    /// Sets the iframe's attribute: ``width``.
+    ///
+    /// - Parameters:
+    ///   - width: The width of the element, in pixels.
     func width(_ width: Double) -> iFrame {
         var copy = self
         copy.attributes["width"] = "\(width)"
