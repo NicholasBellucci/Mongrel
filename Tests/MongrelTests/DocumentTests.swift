1c5f04db1332aabaface3821aee2746bf3495b3f
Nicholas Bellucci <nickdbellucci@gmail.com>
2021-07-21 12:36:52 -0400

    Minor updates

--- Tests/MongrelTests/DocumentTests.swift
+++ Tests/MongrelTests/DocumentTests.swift
@@ -2,5 +2,11 @@ import XCTest
 @testable import Mongrel
 
 final class DocumentTests: XCTestCase {
-    
+    func testDocument() {
+        let document = Root {
+
+        }
+
+        print(document)
+    }
 }
