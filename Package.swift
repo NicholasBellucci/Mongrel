2f5ade1ffdc22ac414e99e73b0023a5fe96a2ee9
Nicholas Bellucci <nickdbellucci@gmail.com>
2021-07-21 12:38:28 -0400

    Updated package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Mongrel",
+    name: "mongrel",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
